5d4071a6995847bd4929b3a960f37f809f41c89f
Sofyan Egi <sofyanegil@gmail.com>
2022-08-18 20:36:02 +0700

    refactor: implemented proptypes

--- contacts-app/src/components/ContactInput.js
+++ contacts-app/src/components/ContactInput.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class ContactInput extends React.Component {
   constructor(props) {
@@ -8,7 +9,7 @@ class ContactInput extends React.Component {
     this.state = {
       name: '',
       tag: '',
-    }
+    };
 
     this.onNameChangeEventHandler = this.onNameChangeEventHandler.bind(this);
     this.onTagChangeEventHandler = this.onTagChangeEventHandler.bind(this);
@@ -19,7 +20,7 @@ class ContactInput extends React.Component {
     this.setState(() => {
       return {
         name: event.target.value,
-      }
+      };
     });
   }
 
@@ -27,7 +28,7 @@ class ContactInput extends React.Component {
     this.setState(() => {
       return {
         tag: event.target.value,
-      }
+      };
     });
   }
 
@@ -37,14 +38,18 @@ class ContactInput extends React.Component {
   }
 
   render() {
-   return (
-     <form className='contact-input' onSubmit={this.onSubmitEventHandler}>
-       <input type="text" placeholder="Nama" value={this.state.name} onChange={this.onNameChangeEventHandler} />
-       <input type="text" placeholder="Tag" value={this.state.tag} onChange={this.onTagChangeEventHandler} />
-       <button type="submit">Tambah</button>
-     </form>
-   )
- }
+    return (
+      <form className="contact-input" onSubmit={this.onSubmitEventHandler}>
+        <input type="text" placeholder="Nama" value={this.state.name} onChange={this.onNameChangeEventHandler} />
+        <input type="text" placeholder="Tag" value={this.state.tag} onChange={this.onTagChangeEventHandler} />
+        <button type="submit">Tambah</button>
+      </form>
+    );
+  }
 }
+
+ContactInput.propTypes = {
+  addContact: PropTypes.func.isRequired,
+};
 
 export default ContactInput;
